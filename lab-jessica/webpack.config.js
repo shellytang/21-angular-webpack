80ccc4685b622ad88a93c2cb8b2ac62ec0a200dc
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-02-01 20:08:56 -0800

    Scaffolding

---
+++ lab-jessica/webpack.config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let HTMLPlugin = require ('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/entry.js',
+  output: {
+    filename: 'bundle.js',
+    path: './build'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HTMLPlugin()
+  ]
+};
