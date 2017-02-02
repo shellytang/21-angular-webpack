d3556ed12b12672c88d91692600dcbaa76178e8d
Kyle Winckler <kyle.winckler@gmail.com>
2017-02-01 20:51:29 -0800

    works

--- lab-kyle/webpack.config.js
+++ lab-kyle/webpack.config.js
@@ -9,18 +9,18 @@ module.exports = {
   plugins: [
     new HTMLPlugin({
       template: './app/index.html'
-    })
+    }),
   ],
   module: {
     loaders: [
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.js$/,
-        use: ['eslint']
       }
+      // {
+      //   test: /\.js$/,
+      //   use: ['eslint']
+      // }
     ]
   }
 };
