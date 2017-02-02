0339f0e9adc22f0e057ca61e0ad2f0dbd3dafc80
Dan Peters <daniel.r.peters@icloud.com>
2017-02-01 20:51:51 -0800

    set up angular app basics

--- lab-dan/app/index.js
+++ lab-dan/app/index.js
@@ -1,1 +1,9 @@
 const cowsay = require('cowsay')
+require('./scss/main.scss')
+
+angular.module('cowsayModule', [cowsay])
+
+angular.module('cowsayModule').controller('cowsayCtrl', function($scope) {
+  let self = this
+  return self.output = cowsay(self.message)
+})
