09aed105f2049db9004329c2ae4385a652bcc54a
Frank Iriarte <jurrish@gmail.com>
2017-02-04 14:32:25 -0800

    controller as syntax working

--- lab-21-JR/app/index.js
+++ lab-21-JR/app/index.js
@@ -15,21 +15,20 @@ const cowsayApp = angular.module('cowsayApp', []);
 //wherever ng-controller lives in the html, this stuff runs there depending on the {{variableName}}
 //  mounting .controller here - use this, and this is what it's called
 //the controller is instantiated inside the ng directive element.
-cowsayApp.controller('CowsayController', ['$log', '$scope', CowsayController]);
+cowsayApp.controller('CowsayController', ['$log', CowsayController]);
 //$log and $scope are native SERVICES
 
 
 //defines CowsayController and what properties are related to it
 //we use controllers to control scope, as well as add functionality within that scoped html element. this allows for modular commonJS functionality, and the ability to work on particular elements.
-function CowsayController($log, $scope){
+function CowsayController($log){
   $log.debug('doing CowsayController stuff');
   //setting up the initial state of our scope object. we attach properties to it
-  let cowsayCtrl = $scope.cowsayCtrl = {};
-  cowsayCtrl.title = 'render a cow';
+  this.title = 'render a cow';
 
-  cowsayCtrl.updateCow = function(input){
+  this.updateCow = function(input){
     $log.debug('cowsayCtrl.updateCow stuff');
     return '\n' + cowsay.say({text: input || 'im gonna say cool stuff'});
   };
-  cowsayCtrl.title2 = 'basic text render';
+  this.title2 = 'basic text render';
 }
--- lab-21-JR/webpack.config.js
+++ lab-21-JR/webpack.config.js
@@ -6,13 +6,18 @@ module.exports = {
   entry: './app/index.js', //where webpack will load the js
   output: {
     filename: 'bundle.js',
-    path: './build'
+    path: `${__dirname}/build`
   },
   module: {
     loaders: [  //file by file
       {
         test: /\.scss$/,  //look through the directory and anything with an extension of .scss, do this stuff to it
         use: ['style-loader', 'css-loader', 'sass-loader'] //these are read right to left.
+      },
+      {  //transpiles here
+        test:/\.js$/,  //turn all .js files into es2015 syntax for older browsers
+        use: ['babel-loader'],  //use babel-loader library
+        exclude: /node_modules/  //dont bother transforming node_modules
       }
     ]
   },
