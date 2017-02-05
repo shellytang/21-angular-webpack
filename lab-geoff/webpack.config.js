e2a163df360b006a0670de765fe1b142bdad5b0f
Geoffrey Cox <gcox3d@gmail.com>
2017-02-05 09:43:12 -0800

    added babelrc and updated webpack.config

--- lab-geoff/webpack.config.js
+++ lab-geoff/webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader']
       }
     ],
   },
