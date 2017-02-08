e62e2f14075c8048e874b3946cf56cc9cd7e5831
Dan Peters <daniel.r.peters@icloud.com>
2017-02-07 19:14:22 -0800

    tests are now working

--- lab-dan/test/app-test.js
+++ lab-dan/test/app-test.js
@@ -4,24 +4,22 @@ require('./lib/test-setup.js')
 const angular = require('angular')
 const cowsay = require('cowsay-browser')
 const defaultMsg = 'Type something!'
+const testMsg = 'Test!'
 
-describe('Cowsay Controller', () => {
+describe('Cowsay Controller', function () {
 
   beforeEach(() => {
     angular.mock.module('cowsayModule')
     angular.mock.inject($controller => {
       this.cowsayCtrl = new $controller('CowsayCtrl')
       this.cowsayCtrl.form = {}
-      this.cowsayCtrl.form.message = 'Test'
+      this.cowsayCtrl.form.message = testMsg
     })
   })
 
   describe('Default Properties', () => {
     it('title is set correctly', () => {
       expect(this.cowsayCtrl.title).toBe('Angular Creature Speak')
-    })
-    it('default message', () => {
-      expect(this.cowsayCtrl.defaultMsg).toBe(defaultMsg)
     })
     it('saves array to be an empty array', () => {
       expect(angular.isArray(this.cowsayCtrl.saves)).toBe(true)
@@ -32,30 +30,30 @@ describe('Cowsay Controller', () => {
       expect(this.cowsayCtrl.creatures.length > 0).toBe(true)
     })
     it('default output to equal default message', () => {
-      expect(this.formOutput).toBe(defaultMsg)
+      expect(this.cowsayCtrl.formOutput).toBe(defaultMsg)
     })
   })
 
   describe('#updateMsg', () => {
-    it('updates formOuput with default message and creature', () => {
-      let expectedMsg = cowsay.say({text: defaultMsg, f:'dragon'})
+    it('updates formOutput with default message and creature', () => {
+      let expectedMsg = cowsay.say({text: testMsg, f:'dragon'})
       this.cowsayCtrl.updateMsg()
       expect(this.cowsayCtrl.formOutput).toBe(expectedMsg)
     })
   })
 
   describe('#save', () => {
-    this.cowsayCtrl.updateMsg()
     it('saves message to array', () => {
+      this.cowsayCtrl.updateMsg()
       this.cowsayCtrl.save()
       expect(this.cowsayCtrl.saves.length).toBe(1)
     })
   })
 
   describe('#undo', () => {
-    this.cowsayCtrl.updateMsg()
-    this.cowsayCtrl.save()
     it('removes save from array', () => {
+      this.cowsayCtrl.updateMsg()
+      this.cowsayCtrl.save()
       expect(this.cowsayCtrl.saves.length).toBe(1)
       this.cowsayCtrl.undo()
       expect(this.cowsayCtrl.saves.length).toBe(0)
@@ -64,13 +62,16 @@ describe('Cowsay Controller', () => {
 
   describe('#reset', () => {
     it('resets formOutput to default', () => {
+      this.cowsayCtrl.reset()
       expect(this.cowsayCtrl.formOutput).toBe(defaultMsg)
     })
     it('empties the saves array', () => {
+      this.cowsayCtrl.reset()
       expect(this.cowsayCtrl.saves.length).toBe(0)
     })
     it('resets the form message to an empty string', () => {
-      expect(this.form.message).toBe('')
+      this.cowsayCtrl.reset()
+      expect(this.cowsayCtrl.form.message).toBe('')
     })
   })
 })
