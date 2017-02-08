6431c5d9bd01cb084dfa4c6545a22586c8b534ba
Geoffrey Cox <gcox3d@gmail.com>
2017-02-07 16:05:42 -0800

    tests up for initial properties

--- lab-geoff/test/test.js
+++ lab-geoff/test/test.js
@@ -3,6 +3,8 @@ require('angular-mocks');
 let angular = require('angular');
 let cowsay = require('cowsay-browser');
 
+let options = ['beavis.zen', 'bud-frogs', 'bunny', 'cheese', 'chick', 'chicken-and-egg', 'cower', 'daemon', 'default', 'doge', 'dragon-and-cow', 'dragon', 'elephant-in-snake', 'elephant', 'eyes', 'flaming-sheep', 'ghostbusters', 'hellokitty', 'kiss', 'kitty', 'koala', 'kosh', 'luke-koala', 'mech-and-cow', 'meow', 'milk', 'moofasa', 'moose', 'mutilated', 'ren', 'rooster', 'satanic', 'sheep', 'skeleton', 'small', 'squirrel', 'stegosaurus', 'stimpy', 'supermilker', 'surgery', 'turkey', 'turtle', 'tux', 'vader-koala', 'vader', 'www'];
+
 describe('CowSayController', function() {
   let cowSay = null;
 
@@ -16,6 +18,18 @@ describe('CowSayController', function() {
     it('should have a title COW SAY', function() {
       console.log(cowSay);
       expect(cowSay.title).toBe('COW SAY');
+    });
+    it('should have an empty array called cowOptions', function() {
+      expect(cowSay.cowOptions).toEqual(options);
+    });
+    it('cowSaved should be an empty string', function() {
+      expect(cowSay.saved).toBe('');
+    });
+    it('savedCows should be an empty array', function() {
+      expect(cowSay.savedCows).toEqual([]);
+    });
+    it('the property custom should be set to default', function() {
+      expect(cowSay.custom).toBe('default');
     });
   });
 });
