9a527f06ad5a4ee98ff9246ae95c4d90c001fc96
Trenton Kress <trenton.kress@gmail.com>
2017-02-14 18:01:27 -0800

    Refactor for the 'Controller as' syntax.

--- lab-trent/app/index.js
+++ lab-trent/app/index.js
@@ -7,16 +7,14 @@ const cowsay = require('cowsay-browser');
 
 const cowsayApp = angular.module('cowsayApp', []);
 
-cowsayApp.controller('CowsayController', ['$log', '$scope', CowsayController]);
+cowsayApp.controller('CowsayController', ['$log', CowsayController]);
 
-function CowsayController($log, $scope) {
-  $scope.cowsayCtrl = {};
-
-  $scope.cowsayCtrl.cowify = function(input) {
+function CowsayController($log) {
+  this.cowify = function(input) {
     return '\n' + cowsay.say({text: input || ' '});
   };
 
-  $scope.cowsayCtrl.save = function() {
+  this.save = function() {
 
   };
 }
