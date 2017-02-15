e071c039c9f7ff44efbb645d67b12ad1c5c7c3e0
Trenton Kress <trenton.kress@gmail.com>
2017-02-14 17:28:59 -0800

    Set up the angular module.

---
+++ lab-trent/app/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+require('./scss/main.scss');
+
+const angular = require('angular');
+const cowsay = require('cowsay-browser');
+
+const cowsayApp = angular.module('cowsayApp', []);
+
+cowsayApp.controller('CowsayController', ['$log', '$scope' CowsayController]);
+
+function CowsayController($log, $scope) {
+  $scope.cowsayCtrl = {};
+
+  cowsayCtrl.cowify = function(input) {
+    return '\n' + cowsay.say({text: input || ' '});
+  };
+
+  cowsayCtrl.save() = function() {
+
+  };
+}
