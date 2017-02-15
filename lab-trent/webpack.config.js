9ee1cad658bbd6d32123716ff56be930915485ec
Trenton Kress <trenton.kress@gmail.com>
2017-02-14 17:06:26 -0800

    Set up webpack and sass.

--- lab-trent/webpack.config.js
+++ lab-trent/webpack.config.js
@@ -1,7 +1,20 @@
+let HTMLPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './app/index',
   output: {
     filename: 'bundle.js',
     path: './build'
   },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HTMLPlugin()
+  ]
 };
